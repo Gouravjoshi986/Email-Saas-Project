033e88bfe2c149a8645ac6ac7648c1f2582690a4
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-11-23 01:25:55 +0530

    Created t3 app and added shadcn components

---
+++ normalhuman/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "normalhuman/n/components/ui/button";
+
+export default async function Home() {
+  return (
+    <>
+    <h1 className="text-red-600">Hello world</h1>
+    <Button>Hello World</Button>
+    </>
+  )
+}
