9cf3b9f39e395cce1db82104adbb028a4a73738f
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-11-30 17:37:45 +0530

    added clerk auth

--- normalhuman/src/app/page.tsx
+++ normalhuman/src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "normalhuman/n/components/ui/button";
-
+import { ClerkProvider } from "@clerk/nextjs";
 export default async function Home() {
   return (
-    <>
+    <ClerkProvider>
     <h1 className="text-red-600">Hello world</h1>
     <Button>Hello World</Button>
-    </>
+    </ClerkProvider>
   )
 }
